cff26dadeff53dbe946c98a033bd1d72cc049353
vishalsinghshekhawat18-glitch <vishalsinghshekhawat18-glitch@users.noreply.github.com>
2026-09-12 14:45:44 +0000

    deploy: 71b89ed5bdabb9387c89b48d2829d27099e2f329

---
+++ _next/static/nxoKWjFQIsMw0Oa_sZx7W/_ssgManifest.js
@@ -0,0 +1,1 @@
+self.__SSG_MANIFEST=new Set(["\u002Fconcepts\u002F[slug]","\u002Fsubjects\u002F[slug]","\u002Ftopics\u002F[slug]","\u002Ftopics\u002F[slug]\u002Fread"]);self.__SSG_MANIFEST_CB&&self.__SSG_MANIFEST_CB()
